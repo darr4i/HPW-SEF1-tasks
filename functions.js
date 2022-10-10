11021385cd750d1a055df04c0e0ec42d9caa4aae
Daria Rabiychuk <115396338+darr4i@users.noreply.github.com>
2022-10-10 09:19:53 +0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,13 @@
+for (i = 0; i < 10; i++) {
+   let a, b;
+   a = square = (i*i);
+   b = cube = (i*i*i);
+
+   console.log(square, cube);
+
+   average = ((a + b)/2);
+   console.log(average);
+
+   let calculate = [average];
+   console.log(calculate);
+};
