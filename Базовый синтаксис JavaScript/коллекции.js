996aee3e704a9c28c252e6335d59294488777df3
Daria Rabiychuk <115396338+darr4i@users.noreply.github.com>
2022-11-22 19:40:22 +0200

    Create коллекции.js

---
+++ Базовый синтаксис JavaScript/коллекции.js
@@ -0,0 +1,13 @@
+telephoneBook = [
+    {name: 'Valera Vasilkov', phone: '+380956777890' },
+    {name: 'Katya Yakovleva', phone: '+38067895643'},
+    {name: 'Svetlana Boyko', phone: '+38044786543'},
+    {name: 'Oleksandr Kirov', phone: '+380956789564'},
+    {name: 'Pasha Svetlanov', phone: '+380967895673'}
+];
+
+const findPhoneByName = (name) => {
+    for (const obj of telephoneBook) {
+        if (obj.name === name) return obj.phone;
+    }
+};
